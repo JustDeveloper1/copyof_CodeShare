a30e3bf6373b475e397d15cb73dbc58e3af9a135
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-01-13 21:18:02 +0300

    Update app1.js.tsx

---
+++ pages/js/app1.js.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import data from "../src/js/app1.js"
+
+const Home: NextPage = () => {
+  return (
+    data
+  );
+};
+
+export default Home;
